4bcb65586b2a6373c08f2f952148859926616902
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-01-04 16:47:27 +0200

    Added session checking for all admin pages

--- admin-pages/songs.php
+++ admin-pages/songs.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['id'])) {
+    header("Location: /pages/admin-login.php");
+    exit();
+}
+
 require_once('../include/config.php');
 $album=$_GET['album'];
 
