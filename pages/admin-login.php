38402b26172f2f8a433539912d58634f0c4b5af5
7ermi <7erminatrontzou7@gmail.com>
2023-01-06 00:43:47 +0200

    Fixed admin-login design

--- pages/admin-login.php
+++ pages/admin-login.php
@@ -17,24 +17,25 @@ require('../include/config.php');
     <div class="card mb-3">
         <div class="row g-0 d-flex align-items-center">
             <div class="col-lg-5 d-none d-lg-flex">
-                <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
+                <img src="../imgs/login.jpg" alt="Login Image"
                      class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
             </div>
-            <div class="col-lg-6">
-                <h1>Login</h1>
+            <div class="col-lg-6 login-container">
+                <h1>Welcome!</h1>
+                <h2>Login</h2>
 
                 <div class="card-body py-5 px-md-5">
                     <form action="../include/loginLogic.php" method="post">
                         <!-- Email input -->
                         <div class="form-outline mb-4">
-                            <input type="text"   class="form-control" name="username" />
                             <label id="username" class="form-label" >Username</label>
+                            <input type="text"   class="form-control" name="username" />
                         </div>
 
                         <!-- Password input -->
                         <div class="form-outline mb-4">
-                            <input type="password"  class="form-control" name="password"/>
                             <label id="password" class="form-label" >Password</label>
+                            <input type="password"  class="form-control" name="password"/>
                         </div>
 
                         <?php
